5fe4fa5dd96eaa80a98637b260617209f98308cb
Bhavya Teja <bhavyateja7@gmail.com>
2017-09-19 21:31:05 -0500

    Comments updated.

--- Lab_2/Source/Task_2.py
+++ Lab_2/Source/Task_2.py
@@ -1,9 +1,24 @@
+"""
+Python program to generate a dictionary that contains (k, k*k).
+
+And printing the dictionary that is generated including both 1 and k.
+
+"""
+
+# Taking the input
+
 num = int(input("Input a number "))
+
+# Initialising the dictionary
 
 dictionary = dict()
 
+# Computing the k*k using a for loop
+
 for k in range(1,num+1):
     dictionary[k] = k*k
+
+# Printing the whole dictionary
 
 print(dictionary)
 
--- Lab_2/Source/Task_4.py
+++ Lab_2/Source/Task_4.py
@@ -1,11 +1,25 @@
-#Importing the libraries
+# Using Numpy creating a random vector of size 15 and replace the maximum value by 100
+
+# Importing the libraries
 
 import numpy as np
 
+# Creating a vector with 15 random values
+
 vector = np.random.rand(15)
+
+# Printing the vector
+
+print('The Vector with random values \n')
 
 print(vector)
 
+# Getting the maximum value of the vector and replacing that value with 100
+
 vector[vector.argmax()] = 100
+
+# Printing the new vector
+
+print('The Vector after replacing the maximum value with 100 \n ')
 
 print(vector)
