e5280c6a15c942d13e91e484cc3d120a808e7dce
Bhavya Teja <bhavyateja7@gmail.com>
2017-10-09 18:12:31 -0500

    Code Updated.

--- Lab_3/Task1/Source/LinearRegression.py
+++ Lab_3/Task1/Source/LinearRegression.py
@@ -28,14 +28,6 @@ X_train = np.array(X_train).reshape(-1,1)
 y_train = np.array(y_train).reshape(-1,1)
 X_test = np.array(X_test).reshape(-1,1)
 
-# Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
-sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
-
 # Fitting Simple Linear Regression to the Training set
 
 from sklearn.linear_model import LinearRegression
