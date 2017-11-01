596a860b507a20e787ca7fecccfcab46564ea944
Bhavya Teja <bhavyateja7@gmail.com>
2017-11-01 15:02:50 -0500

    Datafile imported.

--- DL_Lab_1/Source/LogisticRegression.py
+++ DL_Lab_1/Source/LogisticRegression.py
@@ -4,7 +4,14 @@ import tensorflow as tf
 import xlrd
 from mpl_toolkits.mplot3d import Axes3D
 
+dataset = '/Users/bhavyateja/Github_Projects/Python-DeepLearning_CS5590/DL_Lab_1/Documentation/Dataset/dataset_Facebook.xls'
 
+# Step 1: read in data from the .xls file
+
+book = xlrd.open_workbook(dataset, encoding_override="utf-8")
+sheet = book.sheet_by_index(0)
+data = np.asarray([sheet.row_values(i) for i in range(1, sheet.nrows)])
+n_samples = sheet.nrows - 1
 
 
 
